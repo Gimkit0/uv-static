be21d3c90fbb8166d5b730b52f680cc3283fdb9f
Server <161137675+Gimkit0@users.noreply.github.com>
2025-03-15 19:04:52 -0500

    Delete uv.js

--- uv.js
+++
@@ -1,8 +0,0 @@
-importScripts('/uv.bundle.js');
-importScripts('/uv.config.js');
-importScripts('/uv.sw.js');
-importScripts('https://arc.io/arc-sw-core.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
