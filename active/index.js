7fa463a5cab4341c59de95bcfb4b5a91e9cf0d18
Server <161137675+Gimkit0@users.noreply.github.com>
2025-03-19 22:23:10 -0500

    Update index.js

--- active/index.js
+++ active/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // register-sw.js
-const stockSW = "https://gimkit0.github.io/uv-static/active/uv-sw.js";
+const stockSW = "s.js";
 const swAllowedHostnames = ["localhost", "127.0.0.1"];
 
 async function registerSW() {
