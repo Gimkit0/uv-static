84916cec0acf8c5dde86ef332395a55dbfa67cb0
Server <161137675+Gimkit0@users.noreply.github.com>
2025-03-19 21:17:36 -0500

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/active/uv/uv.handler.js',
-    bundle: '/active/uv/uv.bundle.js',
-    config: '/active/uv/uv.config.js',
-    sw: '/active/uv/uv.sw.js',
+    handler: 'https://gimkit0.github.io/uv-static/active/uv/uv.handler.js',
+    bundle: 'https://gimkit0.github.io/uv-static/active/uv/uv.bundle.js',
+    config: 'https://gimkit0.github.io/uv-static/active/uv/uv.config.js',
+    sw: 'https://gimkit0.github.io/uv-static/active/uv/uv.sw.js',
 };
