f59132ed4e99385aaf73e387f552d5072db29582
Server <161137675+Gimkit0@users.noreply.github.com>
2025-04-25 17:36:27 -0500

    Create register.js

---
+++ active/register.js
@@ -0,0 +1,33 @@
+const uvSW = "https://gimkit0.github.io/uv-static/active/uv/uv.sw.js";
+const uvBundle = "https://gimkit0.github.io/uv-static/active/uv/uv.bundle.js";
+
+const swAllowedHostnames = ["localhost", "127.0.0.1"];
+async function registerSW() {
+    if (!('serviceWorker' in navigator)) return;
+
+    await navigator.serviceWorker.register("script.js", {
+        scope: __uv$config.prefix
+    });
+}
+
+var cacheName = 'LVcog';
+var filesToCache = [
+    '/js/sw.js'
+];
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
