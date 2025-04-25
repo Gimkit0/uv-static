11eb5f2d390306ce254370650dacaf03edcfb0ce
Server <161137675+Gimkit0@users.noreply.github.com>
2025-04-25 17:45:13 -0500

    Create script.js

---
+++ active/script.js
@@ -0,0 +1,1 @@
+importScripts("https://gimkit0.github.io/uv-static/active/uv/uv.bundle.js"),importScripts("https://gimkit0.github.io/uv-static/active/uv/uv.sw.js");const s=new UVServiceWorker;self.addEventListener("fetch",t=>t.respondWith(s.fetch(t)));
