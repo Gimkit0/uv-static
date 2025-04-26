d2b6cc66be6019901a96f2ac9a4b890a2dd73cf6
Server <161137675+Gimkit0@users.noreply.github.com>
2025-04-26 11:35:18 -0500

    Update uv.sw.js

--- active/uv/uv.sw.js
+++ active/uv/uv.sw.js
@@ -1,32 +1,6 @@
 importScripts('https://gimkit0.github.io/uv-static/active/uv/uv.bundle.js');
 importScripts('https://gimkit0.github.io/uv-static/active/uv/uv.config.js');
 
-function getUserKey() {
-  const storageKey = 'user-key';
-  const savedKey = localStorage.getItem(storageKey);
-
-  if (savedKey) {
-    return savedKey;
-  }
-
-  let newKey;
-  if (typeof crypto !== 'undefined' && typeof crypto.randomUUID === 'function') {
-    newKey = crypto.randomUUID();
-  } else {
-    newKey = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-      const r = Math.random() * 16 | 0;
-      const v = c === 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
-  }
-
-  localStorage.setItem(storageKey, newKey);
-  return newKey;
-}
-
-let userKey = getUserKey();
-console.log(userKey)
-
 class UVServiceWorker extends EventEmitter {   
     constructor(config = __uv$config) {
         super();
