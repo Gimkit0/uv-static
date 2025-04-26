869526467a144523ce5f0d08610de29e9878b19b
Server <161137675+Gimkit0@users.noreply.github.com>
2025-04-26 09:08:43 -0500

    Delete active/uv-sw.js

--- active/uv-sw.js
+++
@@ -1,5 +0,0 @@
-importScripts("https://gimkit0.github.io/uv-static/active/uv/uv.sw.js");
-
-const sw = new UVServiceWorker();
-
-self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
