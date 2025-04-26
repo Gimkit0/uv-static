78ca1d69a8f117a8e765e09dd0cbc4945c60bb30
Server <161137675+Gimkit0@users.noreply.github.com>
2025-04-26 12:24:29 -0500

    Update uv.loader.js

--- setup/uv.loader.js
+++ setup/uv.loader.js
@@ -1,3 +1,7 @@
 let userKey = new URL(location).searchParams.get('userkey');
-importScripts("https://gimkit0.github.io/uv-static/active/uv/uv.sw.js","https://gimkit0.github.io/uv-static/active/uv/uv.config.js");
-const s = new UVServiceWorker();self.addEventListener("fetch", (t) => t.respondWith(s.fetch(t)));
+importScripts(
+"https://gimkit0.github.io/uv-static/active/uv/uv.sw.js",
+"https://gimkit0.github.io/uv-static/active/uv/uv.config.js"
+);
+const s = new UVServiceWorker();
+self.addEventListener("fetch", (t) => t.respondWith(s.fetch(t)));
