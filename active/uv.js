9293ecdcad230f4936a7e1aa5f70891c532260cb
Server <161137675+Gimkit0@users.noreply.github.com>
2025-04-26 09:08:29 -0500

    Delete active/uv.js

--- active/uv.js
+++
@@ -1,8 +0,0 @@
-importScripts('https://gimkit0.github.io/uv-static/active/uv/uv.bundle.js');
-importScripts('https://gimkit0.github.io/uv-static/active/uv/uv.config.js');
-importScripts('https://gimkit0.github.io/uv-static/active/uv/uv.sw.js');
-importScripts('https://arc.io/arc-sw-core.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
