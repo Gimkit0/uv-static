3c9a370a8c572b1914a1da4d591046eb28fa8d21
Server <161137675+Gimkit0@users.noreply.github.com>
2025-04-26 14:40:40 -0500

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
+    bare:'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: 'https://gimkit0.github.io/uv-static/active/uv/uv.handler.js',
