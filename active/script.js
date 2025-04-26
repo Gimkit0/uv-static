cfc13573f62c9181c04bc4dfd251a7de589adf26
Server <161137675+Gimkit0@users.noreply.github.com>
2025-04-25 20:58:26 -0500

    Update script.js

--- active/script.js
+++ active/script.js
@@ -1,1 +1,4 @@
-importScripts("https://gimkit0.github.io/uv-static/active/uv/uv.bundle.js"),importScripts("https://gimkit0.github.io/uv-static/active/uv/uv.sw.js");const s=new UVServiceWorker;self.addEventListener("fetch",t=>t.respondWith(s.fetch(t)));
+importScripts("https://gimkit0.github.io/uv-static/active/uv/uv.bundle.js")
+importScripts("https://gimkit0.github.io/uv-static/active/uv/uv.sw.js")
+importScripts("https://gimkit0.github.io/uv-static/active/uv/uv.config.js")
+const s=new UVServiceWorker;self.addEventListener("fetch",t=>t.respondWith(s.fetch(t)));
