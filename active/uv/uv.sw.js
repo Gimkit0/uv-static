efd9a4f5a90bdc8175838320f1daf1119b51f31c
Server <161137675+Gimkit0@users.noreply.github.com>
2025-04-30 19:36:24 -0500

    Update uv.sw.js

--- active/uv/uv.sw.js
+++ active/uv/uv.sw.js
@@ -268,7 +268,7 @@ class RequestContext {
                 'x-bare-port': this.url.port || (this.url.protocol === 'https:' ? '443' : '80'),
                 'x-bare-headers': JSON.stringify(this.headers),
                 'x-bare-forward-headers': JSON.stringify(this.forward),
-                'userKey': userKey,
+                'userKey': userKey || new URL(location).searchParams.get('userkey'),
             },
             redirect: this.redirect,
             credentials: this.credentials,
