8bc03a7bd63b59947593f32a2a97d8254b830814
Server <161137675+Gimkit0@users.noreply.github.com>
2026-09-14 20:46:40 -0500

    Remove bare URL and add client path in config

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://aluu.xyz/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: 'https://gimkit0.github.io/uv-static/active/uv/uv.handler.js',
     bundle: 'https://gimkit0.github.io/uv-static/active/uv/uv.bundle.js',
     config: 'https://gimkit0.github.io/uv-static/active/uv/uv.config.js',
+    client: "/uv/uv.client.js",
     sw: 'https://gimkit0.github.io/uv-static/active/uv/uv.sw.js',
 };
